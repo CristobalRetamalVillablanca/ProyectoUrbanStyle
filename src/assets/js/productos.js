5f123b78682d47517e5d2b7374832f2d644bf736
Cristobal_rv <cris.retamalv@duocuc.cl>
2025-09-16 23:12:24 -0300

    PRODUCTOS, CARDS ALGUNAS IMAGENES

---
+++ src/assets/js/productos.js
@@ -0,0 +1,66 @@
+const productos = [
+  {
+    codigo: "HD001",
+    nombre: "Hoodie Oversize Blackout",
+    precio: 28990,
+    stock: 60,
+    descripcion: "Hoodie negro oversize, 100% algodón.",
+    imagen: "src/assets/img/hoodie-black.png"
+  },
+  {
+    codigo: "HD002",
+    nombre: "Hoodie Oversize Grey Storm",
+    precio: 28990,
+    stock: 80,
+    descripcion: "Hoodie gris claro, corte unisex.",
+    imagen: "src/assets/img/hoodie-grey.png"
+  },
+  {
+    codigo: "PL001",
+    nombre: "Polera Estampada Street Art",
+    precio: 14990,
+    stock: 100,
+    descripcion: "Polera blanca estampada estilo street art.",
+    imagen: "src/assets/img/polera-street.png"
+  },
+  {
+    codigo: "PL002",
+    nombre: "Polera Boxy Fit Minimal Logo",
+    precio: 15990,
+    stock: 120,
+    descripcion: "Boxy fit negra con logo minimalista.",
+    imagen: "./assets/img/polera-minimal.png"
+  },
+  {
+    codigo: "PT001",
+    nombre: "Baggy Jeans Blue Wash",
+    precio: 32990,
+    stock: 70,
+    descripcion: "Jeans baggy azul lavado.",
+    imagen: "./assets/img/jeans-baggy.png"
+  },
+  {
+    codigo: "PT002",
+    nombre: "Cargo Pants Black Utility",
+    precio: 29990,
+    stock: 50,
+    descripcion: "Cargo negro con múltiples bolsillos.",
+    imagen: "./assets/img/cargo-black.png"
+  },
+  {
+    codigo: "GR001",
+    nombre: "Gorra Snapback Classic Black",
+    precio: 12990,
+    stock: 90,
+    descripcion: "Snapback negro clásico.",
+    imagen: "./assets/img/gorra-snapback.png"
+  },
+  {
+    codigo: "GR002",
+    nombre: "Gorra Trucker Logo UrbanStyle",
+    precio: 11990,
+    stock: 60,
+    descripcion: "Trucker negra con logo bordado.",
+    imagen: "./assets/img/gorra-trucker.png"
+  }
+];
