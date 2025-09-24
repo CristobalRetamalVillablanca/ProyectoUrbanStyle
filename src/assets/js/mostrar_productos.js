49618e3f714cfa3354bb3effbd4f47176756746c
Cristobal_rv <cris.retamalv@duocuc.cl>
2025-09-23 22:47:10 -0300

    CARRITO

--- src/assets/js/mostrar_productos.js
+++ src/assets/js/mostrar_productos.js
@@ -8,17 +8,25 @@ document.addEventListener("DOMContentLoaded", () => {
     card.innerHTML = `
       <div class="card h-100 w-100 text-center shadow-sm" style="color: #111111">
         <a href="producto.html?id=${prod.codigo}">
-          <img src="${prod.imagen}" class="card-img-top h-100" alt="${prod.nombre}" style="background-color: #B0B0B0">
+          <img src="${prod.imagen}" class="card-img-top h-100" alt="${prod.nombre}" style="background-color: #B0B0B0; object-fit: cover;">
         </a>
-        <div class="card-body  d-flex flex-column" style="background-color: #B0B0B0">
-          <h5 class="card-title ">
+        <div class="card-body d-flex flex-column" style="background-color: #B0B0B0">
+          <h5 class="card-title">
             <a href="producto.html?id=${prod.codigo}" style="text-decoration: none; color: inherit;">
               ${prod.nombre}
             </a>
           </h5>
           <p class="card-text flex-grow-1">${prod.descripcion}</p>
           <p class="fw-bold">$${prod.precio.toLocaleString("es-CL")}</p>
-          <button class="btn btn-success mt-auto">Agregar al carrito</button>
+          <button class="btn btn-success mt-auto"
+                  onclick="agregarAlCarrito({
+                    id: '${prod.codigo}',
+                    nombre: '${prod.nombre}',
+                    precio: ${prod.precio},
+                    cantidad: 1
+                  })">
+            Agregar al carrito
+          </button>
         </div>
       </div>
     `;
